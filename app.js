96a7641188a131b69249c9ce5e6a8b50aabcc22c
ibraheemhajjar <ibraheemhajjar91@gmail.com>
2022-08-10 14:40:05 +0300

    add logger

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+require('dotenv').config();
+
+const parentRoutes = require('./routes/parent')
+const teacherRoutes = require('./routes/teacher')
+const logger = require('./utils/logger');
+const port = process.env.PORT
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(parentRoutes);
+app.use(teacherRoutes);
 
 
-app.listen(3000)
+app.listen(port, () => {
+    logger.info(`server started at port: ${port}`);
+})
